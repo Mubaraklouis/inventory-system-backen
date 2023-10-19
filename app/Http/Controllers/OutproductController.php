bbd6d0d6cc1f54f0ae32f695c4ba32791d6c4685
Mubarak kual louis <mubaraklouis@gmail.com>
2023-10-19 15:28:49 +0200

    [task:4] make the out product table

---
+++ app/Http/Controllers/OutproductController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Outproduct;
+use App\Http\Requests\StoreOutproductRequest;
+use App\Http\Requests\UpdateOutproductRequest;
+
+class OutproductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreOutproductRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Outproduct $outproduct)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateOutproductRequest $request, Outproduct $outproduct)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Outproduct $outproduct)
+    {
+        //
+    }
+}
---
+++ database/migrations/2023_10_16_155727_create_products_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreign('user_id')->on('users')->references()->cascadeOnDelete();
+            $table->string('name');
+            $table->integer('price');
+            $table->integer('serial_number');
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
